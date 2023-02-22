7a37a52df647f537e9c3934e640b96e51d7c6fa9
Oceane <chameleon10712@users.noreply.github.com>
2023-02-22 17:10:34 +0800

    Create Students.py

---
+++ Lab01/Students.py
@@ -0,0 +1,18 @@
+class Students:
+    name = []
+
+    def set_name(self, user_name):
+        self.name.append(user_name)
+        return len(self.name) - 1
+
+    def get_name(self, user_id):
+        if user_id >= len(self.name):
+            return 'There is no such user'
+        else:
+            return self.name[user_id]
+
+
+if __name__ == '__main__':
+    students = Students()
+    print("id", students.set_name('John'))
+    print("name", students.get_name(0))
