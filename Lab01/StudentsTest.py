b88a85ba38a83301e530a4370af1d82c471f3743
Oceane <chameleon10712@users.noreply.github.com>
2023-02-22 17:11:51 +0800

    Create StudentsTest.py

---
+++ Lab01/StudentsTest.py
@@ -0,0 +1,18 @@
+import unittest
+import Students
+
+class Test(unittest.TestCase):
+    students = Students.Students()
+
+    user_name = ['John', 'Mary','Thomas','Jane']
+    user_id = []
+
+    # test case function to check the Students.set_name function
+    def test_0_set_name(self):
+        #TODO
+        pass
+
+    # test case function to check the Students.get_name function
+    def test_1_get_name(self):
+        #TODO
+        pass
