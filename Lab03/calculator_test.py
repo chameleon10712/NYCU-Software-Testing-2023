4a337a103fb3ada7973e826fa8e74d30469f5847
Oceane <chameleon10712@users.noreply.github.com>
2023-03-07 23:38:46 +0800

    Create calculator_test.py

---
+++ Lab03/calculator_test.py
@@ -0,0 +1,19 @@
+import unittest
+from calculator import Calculator
+
+class ApplicationTest(unittest.TestCase):
+
+    def test_add(self):
+        pass
+
+    def test_divide(self):
+        pass
+
+    def test_sqrt(self):
+        pass
+
+    def test_exp(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
